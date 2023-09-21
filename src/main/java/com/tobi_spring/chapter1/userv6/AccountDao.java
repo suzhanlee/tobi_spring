8257eed6b11b63dc3c43abeff12e548ec8ed0ae7
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-09-21 20:54:54 +0900

    feat : Chapter1 user 기능 ver6

---
+++ src/main/java/com/tobi_spring/chapter1/userv6/AccountDao.java
@@ -0,0 +1,10 @@
+package com.tobi_spring.chapter1.userv6;
+
+public class AccountDao {
+
+    private final ConnectionMaker connectionMaker;
+
+    public AccountDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+}
---
+++ src/main/java/com/tobi_spring/chapter1/userv6/MessageDao.java
@@ -0,0 +1,10 @@
+package com.tobi_spring.chapter1.userv6;
+
+public class MessageDao {
+
+    private final ConnectionMaker connectionMaker;
+
+    public MessageDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+}
