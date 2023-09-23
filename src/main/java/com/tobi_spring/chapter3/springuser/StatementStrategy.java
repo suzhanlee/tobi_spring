070021177864e6e5545465e68cca426e0611d8b3
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-09-23 21:25:10 +0900

    feat : Chapter3 jdbcTemplate으로 보는 template-callback 패턴

---
+++ src/main/java/com/tobi_spring/chapter3/springuser/StatementStrategy.java
@@ -0,0 +1,10 @@
+package com.tobi_spring.chapter3.springuser;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
