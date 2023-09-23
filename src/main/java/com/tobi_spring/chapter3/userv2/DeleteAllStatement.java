ca5befe1317910dc1571bd0343096708c718a086
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-09-23 14:50:19 +0900

    feat : Chapter3 user 전략 패턴

---
+++ src/main/java/com/tobi_spring/chapter3/userv2/DeleteAllStatement.java
@@ -0,0 +1,13 @@
+package com.tobi_spring.chapter3.userv2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteAllStatement implements StatementStrategy {
+
+    @Override
+    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        return c.prepareStatement("delete from users");
+    }
+}
