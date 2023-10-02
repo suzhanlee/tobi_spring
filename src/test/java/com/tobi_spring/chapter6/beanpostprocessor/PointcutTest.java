aff5a758abcc00813fdff380597e5bfd6946a566
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-02 15:11:13 +0900

    test : Chapter6 Pointcut 학습테스트

---
+++ src/test/java/com/tobi_spring/chapter6/beanpostprocessor/PointcutTest.java
@@ -0,0 +1,98 @@
+package com.tobi_spring.chapter6.beanpostprocessor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.tobi_spring.chapter6.proxy.HelloTarget;
+import com.tobi_spring.chapter6.proxyfactorybean.DynamicProxyTest;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+public class PointcutTest {
+
+    static interface Hello {
+
+        String sayHello(String name);
+
+        String sayHi(String name);
+
+        String sayThankYou(String name);
+    }
+
+    static class HelloTarget implements Hello {
+
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi " + name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "Thank you " + name;
+        }
+    }
+
+    static class UpperCaseAdvice implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            // reflection의 Method와 달리 인자로 타깃 오브젝트를 전달할 필요 없다.
+            // MethodInvocation은 메소드 정보와 함께 타깃 오브젝트를 알고 있기 때문이다.
+            String ret = (String) invocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+
+    @Test
+    void classNamePointcutAdvisor() {
+        NameMatchMethodPointcut classMethodPointcut = new NameMatchMethodPointcut() {
+            public ClassFilter getClassFilter() {
+                return new ClassFilter() {
+                    @Override
+                    public boolean matches(Class<?> clazz) {
+                        return clazz.getSimpleName().startsWith("HelloT");
+                    }
+                };
+            }
+        };
+
+        classMethodPointcut.setMappedName("sayH*");
+
+        checkAdviced(new HelloTarget(), classMethodPointcut, true);
+
+        class HelloWorld extends HelloTarget {};
+        checkAdviced(new HelloWorld(), classMethodPointcut, false);
+
+        class HelloToby extends HelloTarget {};
+        checkAdviced(new HelloToby(), classMethodPointcut, true);
+    }
+
+    private void checkAdviced(Object target, Pointcut pointcut, boolean adviced) {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(target);
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UpperCaseAdvice()));
+        Hello proxiedHello = (Hello) pfBean.getObject();
+
+        if (adviced) {
+            assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+            assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+            assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("Thank you Toby");
+        } else {
+            assertThat(proxiedHello.sayHello("Toby")).isEqualTo("Hello Toby");
+            assertThat(proxiedHello.sayHi("Toby")).isEqualTo("Hi Toby");
+            assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("Thank you Toby");
+        }
+
+    }
+
+}
